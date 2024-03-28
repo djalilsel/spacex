b0cf7f8c81e2a95603bebdf87387064908454fe7
djalilsel <selamnia.a.eldjalil@gmail.com>
2024-03-28 03:54:52 +0100

    text animations

--- app/page.jsx
+++ app/page.jsx
@@ -1,50 +1,52 @@
+import Main from "../components/Main";
+import heroBg1 from "../assets/LAUNCHES_Starlink_SLC_40.jpg";
+import heroBg2 from "../assets/Crew_6_Desktop_9857f5c2f0.jpg";
+import heroBg3 from "../assets/Launch_Page_Starlink1_vertical_18_DESKTOP_c851b30656.jpg";
+import heroBg4 from "../assets/Star6_17_091923_DSC_5931_desktop_5d8a7a18c0.jpg";
+import heroBg5 from "../assets/STARSHIP_Testflight_DESKTOP_2b3bea613a.jpg";
+import localFont from "next/font/local";
 
-import Main from '../components/Main'
-import heroBg1 from '../assets/LAUNCHES_Starlink_SLC_40.jpg'
-import heroBg2 from '../assets/Crew_6_Desktop_9857f5c2f0.jpg'
-import heroBg3 from '../assets/Launch_Page_Starlink1_vertical_18_DESKTOP_c851b30656.jpg'
-import heroBg4 from '../assets/Star6_17_091923_DSC_5931_desktop_5d8a7a18c0.jpg'
-import heroBg5 from '../assets/STARSHIP_Testflight_DESKTOP_2b3bea613a.jpg'
-import localFont from 'next/font/local'
-
-const ddib = localFont({ src: '../assets/D-DIN-Bold.woff2'})
+const ddib = localFont({ src: "../assets/D-DIN-Bold.woff2" });
 
 export default function Home() {
-
   const data = [
     {
       bgImage: "LAUNCHES_Starlink_SLC_40.965e1358.jpg",
       title: "UPCOMING LAUNCH",
-      missionTitle: "STARLINK MISSION"
+      missionTitle: "STARLINK MISSION",
     },
     {
       bgImage: "Crew_6_Desktop_9857f5c2f0.7e38ba3e.jpg",
       title: "RECENT LAUNCH",
-      missionTitle: "STARLINK MISSION"
+      missionTitle: "STARLINK MISSION",
     },
     {
-      bgImage: "Launch_Page_Starlink1_vertical_18_DESKTOP_c851b30656.79dae754.jpg",
+      bgImage:
+        "Launch_Page_Starlink1_vertical_18_DESKTOP_c851b30656.79dae754.jpg",
       title: "RECENT LAUNCH",
-      missionTitle: "STARLINK MISSION"
+      missionTitle: "STARLINK MISSION",
     },
     {
       bgImage: "Star6_17_091923_DSC_5931_desktop_5d8a7a18c0.0221daa5.jpg",
       title: "COMPLETED MISSION",
-      missionTitle: "DRAGON AND CREW-6 RETURN TO EARTH"
+      missionTitle: "DRAGON AND CREW-6 RETURN TO EARTH",
     },
     {
       bgImage: "STARSHIP_Testflight_DESKTOP_2b3bea613a.7735750d.jpg",
       title: "",
-      missionTitle: "STARSHIP FLIGHT TEST"
-    }
-  ]
+      missionTitle: "STARSHIP FLIGHT TEST",
+    },
+  ];
 
   const MAIN = data.map((item) => {
-    return <Main bgImage={item.bgImage} title={item.title} missionTitle={item.missionTitle}/>
-  })
-  return (
-    <main>
-      {MAIN}
-    </main>
-  )
+    return (
+      <Main
+        key={item.title}
+        bgImage={item.bgImage}
+        title={item.title}
+        missionTitle={item.missionTitle}
+      />
+    );
+  });
+  return <main>{MAIN}</main>;
 }
--- components/Main.jsx
+++ components/Main.jsx
@@ -1,127 +1,144 @@
-'use client'
-import localFont from 'next/font/local';
-import React, { useEffect, useState } from 'react';
-import { motion, useAnimate } from 'framer-motion';
-import { cubicBezier } from 'framer-motion';
+"use client";
+import localFont from "next/font/local";
+import React, { useEffect, useRef, useState } from "react";
+import { motion, useAnimate, useInView } from "framer-motion";
+import { cubicBezier } from "framer-motion";
 
-const ddibBold = localFont({ src: '../assets/D-DIN-Bold.woff2'})
-const ddib = localFont({ src: '../assets/D-DIN.woff2'})
+const ddibBold = localFont({ src: "../assets/D-DIN-Bold.woff2" });
+const ddib = localFont({ src: "../assets/D-DIN.woff2" });
 
-const Main = ({ bgImage, title, missionTitle}) => {
+const Main = ({ bgImage, title, missionTitle }) => {
+  const [arrow, animate] = useAnimate();
+  const div1 = useRef();
+  const div2 = useRef();
+  const div3 = useRef();
+  const inView1 = useInView(div1);
+  const inView2 = useInView(div2);
+  const inView3 = useInView(div3);
+  const cubic = cubicBezier(0.13, 0.57, 0.44, 0.9);
+  const [animation1, setAnimation1] = useState({ opacity: 0 });
+  const [animation2, setAnimation2] = useState({ opacity: 0 });
+  const [animation3, setAnimation3] = useState({ opacity: 0 });
 
-  // const firstDivAnimation = {
-  //   initial: {
-  //     opacity: 0,
-  //   },
-  //   animate: {
-  //     opacity: 1,
-  //     transition: {
-  //       ease: "linear",
-  //       duration: 0.4,
-  //     }
-  //   }
-  // }
-  // const secondDivAnimation = {
-  //   initial: {
-  //     translateY: 0,
-  //   },
-  //   animate: {
-  //     translateY: 10,
-  //     transition: {
-  //       ease: easing,
-  //       duration: 0.8,
-  //       delay: 0.4
-  //     }
-  //   }
-  // }
-  // const thirdDivAnimation = {
-  //   initial: {
-  //     opacity: 1,
-  //   },
-  //   animate: {
-  //     opacity: 0,
-  //     transition: {
-  //       ease: "linear",
-  //       duration: 0.6,
-  //       delay: 1,
-  //     }
-  //   }
-  // }
+  useEffect(() => {
+    if (inView1) {
+      setAnimation1({ opacity: 1, translateY: 0 });
+    }
+    if (inView2) {
+      setAnimation2({ opacity: 1, translateY: 0 });
+    }
+    if (inView3) {
+      setAnimation3({ opacity: 1, translateY: 0 });
+    }
+  }, [inView1, inView2, inView3]);
 
+  useEffect(() => {
+    setInterval(() => {
+      animate([
+        [
+          arrow.current,
+          {
+            opacity: 1,
+          },
+          {
+            duration: 0.6,
+          },
+        ],
+        [
+          arrow.current,
+          {
+            translateY: 10,
+          },
+          {
+            duration: 1.8,
+            ease: cubicBezier(0.4, 0.4, 0.6, 0.8),
+          },
+        ],
+        [
+          arrow.current,
+          {
+            opacity: 0,
+          },
+          {
+            duration: 0.6,
+          },
+        ],
+        [
+          arrow.current,
+          {
+            translateY: 0,
+          },
+          {
+            duration: 0.4,
+          },
+        ],
+      ]);
+    }, 4000);
+  });
 
-  // const [firstDiv, setFirstDiv] = useState(firstDivAnimation)
-  // const suii = ['sui']
-  // const [secondDiv, setSecondDiv] = useState(secondDivAnimation)
-  // const [thirdDiv, setThirdDiv] = useState(thirdDivAnimation)
-  // const [sui, setSui] = useState(false)
-  // const [ARROW, setARROW] = useState()
-
-  const [arrow, animate] = useAnimate()
-
-  const sequence = () => {
-    animate([
-      [arrow.current, { 
-          opacity: 1
-        }, {
-          duration: 0.6
-        }],
-      [arrow.current, { 
-          translateY: 10,
-        }, {
-          duration: 1.8,
-          ease: cubicBezier(0.40, 0.40, 0.60, 0.80)
-        }],
-      [arrow.current, { 
-          opacity: 0
-        }, {
-          duration: 0.6
-        }],
-      [arrow.current, { 
-          translateY: 0
-        }, {
-          duration: 0.4
-        }],
-    ])
-  }
-
-    useEffect (() => {
-      setInterval(() => {
-        sequence()
-      }, 4000);
-    })
- 
-
-
-    return (
-      <div className={`${ddibBold.className} bg-no-repeat bg-center bg-cover h-screen relative`} style={{ backgroundImage: `url(/_next/static/media/${bgImage})` }}>
-        <div className='absolute bottom-0 left-0 w-full h-fit flex justify-center xl:px-[300px] px-[50px] py-[140px]'>
-          <div className='w-screen max-w-[1400px] flex justify-center'>
-            <div className='w-fit max-w-[520px] h-fit flex flex-col leading-[36px] text-white'>
-              <span className={`${ddib.className} text-[20px] `}>
-                {title}
-              </span>
-              <span className='text-[48px] leading-[48px]'>
-                {missionTitle}
-              </span>
-              <button className='inline-block w-fit min-w-[130px] py-0 px-[15px] border-[2px] mt-[30px] border-white'>
-                <div className='text-[14px] h-[50px] w-[130px] flex justify-center items-center'>
-                  LEARN MORE
-                </div>
-              </button>
-            </div> 
-            <div className='flex-1'></div>
+  return (
+    <div
+      className={`${ddibBold.className} bg-no-repeat bg-center bg-cover h-screen relative`}
+      style={{ backgroundImage: `url(/_next/static/media/${bgImage})` }}
+    >
+      <div className="absolute bottom-0 left-0 w-full h-fit flex justify-center xl:px-[300px] px-[50px] py-[140px]">
+        <div className="w-screen max-w-[1400px] flex justify-center">
+          <div className="w-fit max-w-[520px] h-fit flex flex-col leading-[36px] text-white top-[1000]">
+            <motion.span
+              ref={div1}
+              initial={{ opacity: 0, translateY: 50 }}
+              animate={animation1}
+              transition={{ duration: 0.7, ease: cubic }}
+              className={`${ddib.className} text-[20px]`}
+            >
+              {title}
+            </motion.span>
+            <motion.span
+              ref={div2}
+              initial={{ opacity: 0, translateY: 50 }}
+              animate={animation2}
+              transition={{
+                duration: 0.7,
+                ease: cubic,
+              }}
+              className="text-[48px] leading-[48px] "
+            >
+              {missionTitle}
+            </motion.span>
+            <motion.button
+              ref={div3}
+              initial={{ opacity: 0, translateY: 50 }}
+              animate={animation3}
+              transition={{
+                duration: 0.7,
+                ease: cubic,
+                delay: 0.05,
+              }}
+              className="inline-block w-fit min-w-[130px] py-0 px-[15px] border-[2px] mt-[30px] border-white "
+            >
+              <div className="text-[14px] h-[50px] w-[130px] flex justify-center items-center ">
+                LEARN MORE
+              </div>
+            </motion.button>
           </div>
-        </div>
-        <div className='absolute bottom-16 left-1/2 opacity-0 cursor-pointer'
-          ref={arrow}
-          onClick={sequence}
-        >
-                    <svg width="30px" height="20px" aria-label="more content below">
-                      <path fillOpacity="0" stroke="#ffffff" stroke-width="2px" d="M2.000,5.000 L15.000,18.000 L28.000,5.000 "></path>
-                    </svg>
+          <div className="flex-1"></div>
         </div>
       </div>
-    );
+      <div
+        className="absolute bottom-16 left-1/2 opacity-0 select-none"
+        ref={arrow}
+      >
+        <svg width="30px" height="20px" aria-label="more content below">
+          <path
+            fillOpacity="0"
+            stroke="#ffffff"
+            strokeWidth="2px"
+            d="M2.000,5.000 L15.000,18.000 L28.000,5.000 "
+          ></path>
+        </svg>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
